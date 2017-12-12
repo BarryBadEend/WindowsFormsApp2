2ab0fca3ff82ee3220a109abce3d21c4f27171f8
BarryBadEend <33729936+BarryBadEend@users.noreply.github.com>
2017-12-12 14:29:51 +0100

    v1.0

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -16,5 +16,26 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
+    
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            int p;
+            Pen blackPen = new Pen(Brushes.Black);
+
+            for (int y = 0; y <= 6; ++y)
+            {
+                e.Graphics.DrawLine(blackPen, 0, y * 50, 6 * 50, y * 50);
+            }
+
+            for (int x = 0; x <= 6; ++x)
+            {
+                e.Graphics.DrawLine(blackPen, x * 50, 0, x * 50, 6 * 50);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
